35854751d2805b71a028ce1d96a5b26b7d8be678
Martin Underhill <martin@tempertemper.net>
2015-02-06 21:58:06 +0000

    updates to perch blog 4.5.4

--- cms/addons/apps/perch_blog/PerchBlog_Util.class.php
+++ cms/addons/apps/perch_blog/PerchBlog_Util.class.php
@@ -368,7 +368,8 @@ class PerchBlog_Util extends PerchAPI_Factory
 	                        break;
 	                        
 	                    case 'category':
-	                        $Category = $Categories->find_or_create('blog/'.$slug, $label);
+	                    	PerchUtil::debug("Find or create $slug", 'notice');
+	                        $Category = $Categories->find_or_create('blog/'.$slug.'/', $label);
 	                        if (is_object($Category)) {
 	                            $cat_ids[] = $Category->id();
 
@@ -387,7 +388,8 @@ class PerchBlog_Util extends PerchAPI_Factory
 	            }
 
 	            if (PerchUtil::count($cat_ids)) {
-	            	$post['categories'] = $cat_ids;
+	            	$fields['categories'] = $cat_ids;
+	            	$post['postDynamicFields'] = PerchUtil::json_safe_encode($fields);
 	            }
 
 	            $Post->Template = $Template;
