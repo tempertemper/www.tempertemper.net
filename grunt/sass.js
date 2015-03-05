0dd18832728b1e859b10afb13f5d7dea147be580
Martin Underhill <martin@tempertemper.net>
2015-03-05 17:56:28 +0000

    adds sourcemaps to sass output and compresses css

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,8 +1,10 @@
 module.exports = {
   dist: {
     options: {
-      style: 'expanded',
-      require: 'susy'
+      style: 'compressed',
+      require: 'susy',
+      outFile: 'cms/addons/feathers/emspublishing/css/style.css',
+      sourceMap: true,
     },
     files: {
       'cms/addons/feathers/tempertemper/css/style.css': 'assets/scss/style.scss',
