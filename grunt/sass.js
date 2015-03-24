9c3fac1aa1950df9b0975287fb84455c3321f73c
Martin Underhill <martin@tempertemper.net>
2015-03-24 14:06:05 +0000

    updates sass code

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,10 +1,8 @@
 module.exports = {
   dist: {
     options: {
-      style: 'compressed',
-      require: 'susy',
-      outFile: 'cms/addons/feathers/emspublishing/css/style.css',
-      sourceMap: true,
+      outputStyle: 'compressed',
+      sourceMap: true
     },
     files: {
       'cms/addons/feathers/tempertemper/css/style.css': 'assets/scss/style.scss',
