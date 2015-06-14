821f4e7fb7b5b199b40315fc027543cd4a33685f
Martin Underhill <martin@tempertemper.net>
2015-06-14 14:57:45 +0100

    forgot the cms dir

--- cms/templates/layouts/blog/post_details.php
+++
@@ -1,6 +0,0 @@
-<aside class="post-details">
-
-  <p>Posted <span class="entry-published date" ><?php echo strftime("%d %B, %Y", strtotime(perch_blog_post_field(perch_get('s'), 'postDateTime', true))); ?></span> in <span class="cats"><?php perch_blog_post_categories(perch_get('s')); ?></span></p>
-  <p class="tags">Tagged with <?php perch_blog_post_tags(perch_get('s')); ?></p>
-
-</aside>
--- cms/templates/layouts/search_header.php
+++
@@ -1,8 +0,0 @@
-<?php include('partials/_primary_nav.php') ?>
-
-<header role="banner">
-
-  <?php perch_search_form(); ?>
-
-  <?php include('partials/_nav_toggle.php') ?>
-  <?php include('partials/_logo.php') ?>
