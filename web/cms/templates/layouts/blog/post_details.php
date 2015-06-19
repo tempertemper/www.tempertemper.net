81c39bb63a604780bc69e8f3fa2c5f1bb5c44b2a
Martin Underhill <martin@tempertemper.net>
2015-06-19 14:38:12 +0100

    post tags and cats logic reworked

--- web/cms/templates/layouts/blog/post_details.php
+++ web/cms/templates/layouts/blog/post_details.php
@@ -1,7 +1,7 @@
 <aside class="details">
 
-  <p>Posted <span class="entry-published date" ><?php echo strftime("%d %B, %Y", strtotime(perch_blog_post_field(perch_get('s'), 'postDateTime', true))); ?></span> in <span class="cats"><?php perch_blog_post_categories(perch_get('s')); ?></span></p>
-  <p class="tags">Tagged with <?php perch_blog_post_tags(perch_get('s')); ?></p>
+  <p>Posted <span class="entry-published date" ><?php echo strftime("%d %B, %Y", strtotime(perch_blog_post_field(perch_get('s'), 'postDateTime', true))); ?></span><?php perch_blog_post_categories(perch_get('s')); ?></p>
+  <?php perch_blog_post_tags(perch_get('s')); ?>
   <p><a href="/blog/" class="back">Back to blog</a></p>
 
 </aside>
