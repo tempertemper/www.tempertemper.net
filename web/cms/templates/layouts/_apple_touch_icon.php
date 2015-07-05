6944461585e4af0ff022d706dff6de0d5e9fe17a
Martin Underhill <martin@tempertemper.net>
2015-07-05 20:21:17 +0100

    fixes broken icon filepaths

--- web/cms/templates/layouts/_apple_touch_icon.php
+++ web/cms/templates/layouts/_apple_touch_icon.php
@@ -1,6 +1,6 @@
-  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/images/icons/apple-touch-icon-57x57-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/images/icons/apple-touch-icon-114x114-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/images/icons/apple-touch-icon-60x60-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/images/icons/apple-touch-icon-120x120-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/images/icons/apple-touch-icon-72x72-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/images/icons/apple-touch-icon-144x144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/icons/apple-touch-icon-57x57-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/icons/apple-touch-icon-114x114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/img/icons/apple-touch-icon-60x60-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/icons/apple-touch-icon-120x120-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/icons/apple-touch-icon-72x72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/icons/apple-touch-icon-144x144-precomposed.png" />
--- web/cms/templates/layouts/_browser_styling.php
+++ web/cms/templates/layouts/_browser_styling.php
@@ -1,2 +1,2 @@
-<link rel="icon" sizes="any" mask href="/assets/images/icons/tempertemper.svg">
+<link rel="icon" sizes="any" mask href="/img/icons/tempertemper.svg">
 <meta name="theme-color" content="#0097DB">
