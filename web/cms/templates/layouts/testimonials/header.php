d898c3d3475ece642e3eed6ecf23174088a88de0
Martin Underhill <martin@tempertemper.net>
2015-08-11 22:21:58 +0100

    creates 404 style fall-backs for blog, services, projects and testimonials. Ajusts testimonials templating slightly to accommodate fall-back

---
+++ web/cms/templates/layouts/testimonials/header.php
@@ -0,0 +1,23 @@
+<header role="banner">
+
+  <?php
+    perch_layout('_logo');
+    perch_layout('_nav_toggle');
+    perch_layout('_primary_nav');
+  ?>
+
+  <h1>
+
+    <?php
+      perch_collection('Testimonials', array(
+        'template' => 'testimonial_header.html',
+        'filter'   => 'slug',
+        'match'    => 'eq',
+        'value'    => perch_get('s'),
+        'count'    => 1,
+      ));
+    ?>
+
+  </h1>
+
+</header>
