efc0f01911ef9eb3ea252da1b1414931d96fc196
Martin Underhill <martin@tempertemper.net>
2016-06-16 18:34:20 +0100

    tidies up some page templates a bit

--- web/cms/templates/pages/projects/index.php
+++ web/cms/templates/pages/projects/index.php
@@ -1,23 +1,15 @@
 <?php
   perch_layout('head');
   perch_layout('header');
-?>
 
-<main role="main">
+  echo '<main role="main">';
+  perch_content('Introduction');
+  perch_collection('Projects', [
+    'template'   => 'project_list.html',
+    'paginate'   => 'true',
+    'count'      => 5,
+  ]);
+  echo '</main>';
 
-  <?php perch_content('Introduction'); ?>
-
-  <?php
-    perch_collection('Projects', [
-      'template'   => 'project_list.html',
-      'paginate'   => 'true',
-      'count'      => 5,
-    ]);
-  ?>
-
-</main>
-
-<?php
   perch_layout('footer');
   perch_layout('end');
-?>
