aa10a27d58895ddae358b6993f1ca4aa76aaa5a3
Martin Underhill <martin@tempertemper.net>
2018-02-17 10:19:02 +0000

    see last commit

--- web/cms/templates/layouts/_logo.php
+++ web/cms/templates/layouts/_logo.php
@@ -1,4 +1,4 @@
 <a href="/" class="logo" title="Return to homepage">
-  <?php include(PERCH_PATH.'/templates/layouts/svg/tempertemper-logo--abbreviated.svg'); ?>
-  <?php include(PERCH_PATH.'/templates/layouts/svg/tempertemper-logo--full.svg'); ?>
+    <?php include(PERCH_PATH.'/templates/layouts/svg/tempertemper-logo--abbreviated.svg'); ?>
+    <?php include(PERCH_PATH.'/templates/layouts/svg/tempertemper-logo--full.svg'); ?>
 </a>
