986b02b4533f25eb645a9b41d17a89654b2bc0bf
Martin Underhill <martin@tempertemper.net>
2018-02-20 21:32:10 +0000

    removes linting temporarily

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,7 +1,8 @@
 module.exports = {
   sass: {
     files: ['dev/scss/**'],
-    tasks: ['postcss:lint', 'sass', 'postcss:build'],
+    // tasks: ['postcss:lint', 'sass', 'postcss:build'],
+    tasks: ['sass', 'postcss:build'],
     options: {
       spawn: false,
       livereload: true,
