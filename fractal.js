beac423daa19bb0d6bb46d0aabc2e65e7e32476b
Martin Underhill <martin@tempertemper.net>
2018-05-08 20:40:04 +0100

    configures fractals with nunjucks

---
+++ fractal.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fractal = module.exports = require('@frctl/fractal').create();
+
+fractal.components.engine('@frctl/nunjucks');
+fractal.components.set('ext', '.nunj');
+
+fractal.set('project.title', 'tempertemper pattern library');
+
+const mandelbrot = require('@frctl/mandelbrot');
+const tempertemperTheme = mandelbrot({
+  favicon: '../img/icons/favicon.ico',
+  skin: "white"
+});
+fractal.web.theme(tempertemperTheme);
+
+fractal.components.set('path', __dirname + '/src/patterns');
+
+fractal.docs.set('path', __dirname + '/src/docs');
+
+fractal.web.set('static.path', __dirname + '/web');
+fractal.web.set('server.syncOptions', {
+  open: false,
+  injectChanges: true,
+  notify: true
+});
+fractal.components.set('default.preview', '@preview');
+fractal.components.set('default.status', 'wip');
+fractal.docs.set('default.status', 'draft');
+fractal.components.set('label', 'Patterns');
+
+// fractal.web.set('builder.dest', __dirname + '/build');
