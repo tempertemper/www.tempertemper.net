91b7a66a8a4ceaf96327faac43eb45021df6fff4
Martin Underhill <martin@tempertemper.net>
2018-05-13 15:54:08 +0100

    some more fractal config

--- fractal.js
+++ fractal.js
@@ -1,33 +1,67 @@
 'use strict';
 
-const fractal = module.exports = require('@frctl/fractal').create();
+const paths = {
+  build: `${__dirname}/build`,
+  src: `${__dirname}/src`,
+  static: `${__dirname}/tmp`
+};
 
-fractal.components.engine('@frctl/nunjucks');
-fractal.components.set('ext', '.njk');
+const fractal = require('@frctl/fractal').create();
 
-fractal.set('project.title', 'tempertemper pattern library');
-
-const mandelbrot = require('@frctl/mandelbrot');
-const tempertemperTheme = mandelbrot({
-  favicon: '../img/icons/favicon.ico',
+const mandelbrot = require('@frctl/mandelbrot')({
+  favicon: '/assets/img/icons/favicon.ico',
   lang: 'en-gb',
   skin: 'white'
 });
-fractal.web.theme(tempertemperTheme);
 
-fractal.components.set('path', __dirname + '/src/patterns');
+const markdown = require('markdown-it')({
+  html: true,
+  xhtmlOut: true,
+  typographer: true
+});
 
-fractal.docs.set('path', __dirname + '/src/docs');
+const nunjucks = require('@frctl/nunjucks')({
+  filters: {
+    markdown(str) {
+      return markdown.render(str);
+    },
+    markdownInline(str) {
+      return markdown.renderInline(str);
+    },
+    slugify(str) {
+      return str.toLowerCase().replace(/[^\w]+/g, '' + '/\s/g','-');
+    }
+  },
+  paths: [`${paths.static}/assets/img/svg`]
+});
 
-fractal.web.set('static.path', __dirname + '/web');
+// Project config
+fractal.set('project.title', 'tempertemper pattern library');
+
+// Components config
+fractal.components.engine(nunjucks);
+fractal.components.set('ext', '.njk');
+fractal.components.set('default.preview', '@preview');
+fractal.components.set('default.status', 'wip');
+fractal.components.set('label', 'Patterns');
+fractal.components.set('path', `${paths.src}/patterns`);
+
+// Docs config
+fractal.docs.engine(nunjucks);
+fractal.docs.set('ext', '.md');
+fractal.docs.set('default.status', 'draft');
+fractal.docs.set('path', `${paths.src}/docs`);
+
+// Web UI config
+fractal.web.theme(mandelbrot);
+fractal.web.set('static.path', paths.static);
 fractal.web.set('server.syncOptions', {
   open: false,
   injectChanges: true,
   notify: true
 });
-fractal.components.set('default.preview', '@preview');
-fractal.components.set('default.status', 'wip');
-fractal.docs.set('default.status', 'draft');
-fractal.components.set('label', 'Patterns');
+fractal.web.set('builder.dest', paths.build);
+fractal.web.set('builder.urls.ext', null);
 
-// fractal.web.set('builder.dest', __dirname + '/build');
+// Export config
+module.exports = fractal;
