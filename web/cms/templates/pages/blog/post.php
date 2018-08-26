449061ddf6f3b10585b1dad3e4eaf507553cccc0
Martin Underhill <martin@tempertemper.net>
2018-08-26 10:31:49 +0100

    puts date at top of each blog post

--- web/cms/templates/pages/blog/post.php
+++ web/cms/templates/pages/blog/post.php
@@ -8,6 +8,7 @@ perch_layout('header', [
 ]);
 perch_blog_check_preview();
 echo '<main role="main" class="entry-content">';
+echo '<aside class="post-details"><p><span class="entry-published date" >', strftime("%e %B %Y", strtotime(perch_blog_post_field(perch_get('s'), 'postDateTime', true))),'</span></p></aside>';
 perch_blog_post(perch_get('s'));
 echo '</main>';
 echo '<div role="complementary">';
