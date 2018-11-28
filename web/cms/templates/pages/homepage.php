e10c5fb24c66ab1faf33737de0e7c69144df2a24
Martin Underhill <martin@tempertemper.net>
2018-11-28 17:37:19 +0000

    removes projects and testimonials from homepage

--- web/cms/templates/pages/homepage.php
+++ web/cms/templates/pages/homepage.php
@@ -9,22 +9,6 @@ perch_blog_custom([
     'sort-order' => 'DESC',
     'count'      => 2,
 ]);
-perch_collection('Projects', [
-    'template' => 'project_teaser.html',
-    'count'=>2,
-]);
-echo '<aside class="testimonials">';
-echo '<h2>Testimonials</h2>';
-perch_collection('Testimonials', [
-    'sort'=>'citation',
-    'sort-order'=>'RAND',
-    'count'=>1,
-    'template'=>'testimonial_feature.html',
-    'filter' => 'featured',
-    'match' => 'eq',
-    'value' => 'featured',
-]);
-echo '</aside>';
 perch_layout('call_to_action');
 echo '</main>';
 perch_layout('footer');
