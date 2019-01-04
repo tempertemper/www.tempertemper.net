ff6b508b58c6f528938cb2e801a632ea428b5773
Martin Underhill <martin@tempertemper.net>
2019-01-04 11:08:02 +0000

    adds json feed page template

---
+++ web/cms/templates/pages/blog/json.php
@@ -0,0 +1,36 @@
+<?php
+    include('../cms/runtime.php');
+
+    $domain = 'https://'.$_SERVER['HTTP_HOST'];
+
+    $feed = [
+        'version'       => 'https://jsonfeed.org/version/1',
+        'title'         => 'tempertemper blog',
+        'home_page_url' => $domain,
+        'feed_url'      => $domain.'/events/feed.json',
+        'icon'          => $domain.'/cms/addons/feathers/voiceintheroom/img/icons/apple-touch-icon-180x180.png',
+        'favicon'       => $domain.'/cms/addons/feathers/voiceintheroom/img/icons/favicon.png',
+        'items'         => [],
+    ];
+
+    $items = perch_blog_custom([
+        'skip-template' => true,
+        'count'         => 10,
+        'sort'          => 'postDateTime',
+        'sort-order'    => 'DESC'
+    ]);
+
+    if (count($items)) {
+        foreach($items as $item) {
+            $feed['items'][] = [
+                'id'             => $item['postID'],
+                'date_published' => date('c', strtotime($item['postDateTime'])),
+                'title'          => $item['postTitle'],
+                'content_html'   => $item['excerpt'],
+                'url'            => $domain.'/events/'.$item['postURL'],
+            ];
+        }
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($feed);
