ccb7a39755050da46ca215c70b313a89d0dd6793
Martin Underhill <martin@tempertemper.net>
2019-01-12 13:42:06 +0000

    eleventy config

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+module.exports = function(eleventyConfig) {
+
+  /* Date filter */
+  eleventyConfig.addFilter("date", require("./filters/dates.js") );
+
+  /* Markdown Plugins */
+  var uslug = require('uslug');
+  var uslugify = s => uslug(s);
+  var anchor = require('markdown-it-anchor');
+  var markdownIt = require("markdown-it");
+  eleventyConfig.setLibrary("md", markdownIt({
+    typographer: true
+  }).use(anchor, {slugify: uslugify}));
+  var mdIntro = markdownIt({
+    typographer: true
+  });
+  eleventyConfig.addFilter("markdown", function(markdown) {
+    return mdIntro.render(markdown);
+  });
+
+  return {
+    dir: {
+      input: "src/site",
+      output: "dist",
+      includes: "_includes"
+    },
+    templateFormats : ["njk", "html", "md"],
+    htmlTemplateEngine : "njk",
+    markdownTemplateEngine : "njk"
+  };
+};
