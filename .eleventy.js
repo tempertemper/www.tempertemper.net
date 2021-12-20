8a5e788be0c0db36c4cab0e86b7595912d0442d1
Martin Underhill <tempertemper@users.noreply.github.com>
2021-12-20 19:52:58 +0000

    Makes local 404s redirect to proper page

--- .eleventy.js
+++ .eleventy.js
@@ -86,6 +86,7 @@ module.exports = eleventyConfig => {
   });
 
   /* Localhost server config */
+  const fs = require("fs");
   eleventyConfig.setBrowserSyncConfig({
     port: 3000,
     watch: true,
@@ -96,7 +97,17 @@ module.exports = eleventyConfig => {
       }
     },
     open: false,
-    notify: false
+    notify: false,
+    callbacks: {
+      ready: function(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
   });
 
   return {
