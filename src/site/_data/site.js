9ea7835655c49f2891af6665370b05431e3382c1
Martin Underhill <tempertemper@users.noreply.github.com>
2022-06-07 19:00:58 +0100

    Bumps version number to 6.3.8

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   "title": "tempertemper",
   "company": "tempertemper Web Design Ltd",
-  "version": "6.3.7",
+  "version": "6.3.8",
   "url": "https://www.tempertemper.net",
   "baseurl": "",
   "repo": "https://github.com/tempertemper/tempertemper-website",
