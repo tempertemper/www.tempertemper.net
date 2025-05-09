caf5d36c9c70dff9635cb0c91e4b9fc4e66a9485
Damien Robson <damien.robson@sage.com>
2025-05-09 21:48:36 +0100

    Tweaks to improve reliability of removal process

--- src/js/codeblock-focus-removal.js
+++ src/js/codeblock-focus-removal.js
@@ -4,7 +4,6 @@ function checkCodeOverflow() {
 
   // If none are found, log an error and return
   if (!codeElements || codeElements.length === 0) {
-    console.error("Element not found");
     return null;
   }
 
@@ -14,15 +13,6 @@ function checkCodeOverflow() {
     const preElement = codeElement.closest("pre");
     // If no parent pre element is found, log an error and return
     if (!preElement) {
-      console.error("No parent pre element found");
-      return null;
-    }
-
-    // Check if the pre element has a tabindex attribute
-    const hasTabIndex = preElement.hasAttribute("tabindex");
-    // If it doesn't, log an error and return
-    if (!hasTabIndex) {
-      console.info("Pre element does not have tabindex");
       return null;
     }
 
@@ -30,20 +20,19 @@ function checkCodeOverflow() {
     const isOverflowing = preElement.scrollWidth > preElement.clientWidth;
 
     // If the pre element is overflowing, set the tabindex to 0
-    if (isOverflowing) {
-      preElement.setAttribute("tabindex", "0");
-    } else {
-      preElement.removeAttribute("tabindex");
-    }
+    preElement.setAttribute("tabindex", isOverflowing ? 0 : -1);
   });
 }
 
 // Run on page load
-document.addEventListener("DOMContentLoaded", checkCodeOverflow());
+document.addEventListener("DOMContentLoaded", checkCodeOverflow);
+// document.addEventListener("load", checkCodeOverflow());
 
 // Also run on window resize
 let resizeTimer;
 window.addEventListener("resize", () => {
   clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(checkCodeOverflow(), 250);
+  resizeTimer = setTimeout(() => {
+    checkCodeOverflow();
+  }, 250);
 });
