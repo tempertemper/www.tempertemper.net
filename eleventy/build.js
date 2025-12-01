11502e5bd98b1c7f984696d694c099366c27ce34
Martin Underhill <tempertemper@users.noreply.github.com>
2025-12-01 16:35:27 +0000

    Builds search with Eleventy

--- eleventy/build.js
+++ eleventy/build.js
@@ -1,6 +1,10 @@
 import fs from "node:fs";
 import * as esbuild from "esbuild";
 import * as sass from "sass";
+import { exec as _exec } from "node:child_process";
+import { promisify } from "node:util";
+
+const exec = promisify(_exec);
 
 export default function(config) {
 
@@ -28,7 +32,7 @@ export default function(config) {
     console.log("✓ Critical CSS → src/site/_includes/critical.css");
   });
 
-  // Build non-Critical CSS
+  // Build non-Critical CSS and search index
   config.on("eleventy.after", async () => {
     const result = sass.compile("src/scss/non-critical.scss", {
       loadPaths: ["src/scss"],
@@ -40,5 +44,9 @@ export default function(config) {
       result.css
     );
     console.log("✓ Non-critical CSS → dist/assets/css/non-critical.css");
+
+    // Build search index
+    await exec('npx pagefind --site dist --glob "**/*.html"');
+    console.log("✓ Search index → dist/pagefind");
   });
 }
