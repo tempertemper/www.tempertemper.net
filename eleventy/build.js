2c6b5c25359b38fe91121576cbed83aeb5e98624
Martin Underhill <tempertemper@users.noreply.github.com>
2025-12-01 15:19:10 +0000

    Moves JavaScript build to Eleventy config

--- eleventy/build.js
+++ eleventy/build.js
@@ -1,7 +1,19 @@
 import { execSync } from "node:child_process";
+import * as esbuild from "esbuild";
 
-export default function (config) {
-  // config.on('eleventy.after', () => {
-  //   execSync(`npx pagefind --site dist --glob "**/*.html"`, { encoding: 'utf-8' });
-  // });
+export default function(config) {
+
+  // Build JavaScript
+  config.on("eleventy.before", async () => {
+    await esbuild.build({
+      entryPoints: ["src/js/scripts.js"],
+      bundle: true,
+      minify: true,
+      sourcemap: false,
+      outfile: "src/site/_includes/scripts.js",
+      format: "iife",
+      logLevel: "silent"
+    });
+    console.log("✓ JS bundled → src/site/_includes/scripts.js");
+  });
 }
